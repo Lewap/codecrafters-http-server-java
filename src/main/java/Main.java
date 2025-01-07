f9af469204963c7b8c2d1eb24e8cbc28eef1e177
lewap <lewap02@gmail.com>
2025-01-07 17:36:27 +0100

    FS3 user agent

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,10 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-
-    private static PrintWriter out;
-    private static BufferedReader in;
 
     public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -27,28 +25,53 @@ public class Main {
        Socket socket = serverSocket.accept();
        System.out.println("accepted new connection");
 
-       in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-       String clientInput = in.readLine();
+       BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+       String clientInputLine;
+       List<String> requestLines = new ArrayList<>();
+
+       while (!(clientInputLine = in.readLine()).equals("")) {
+           System.out.println("READING request line and saving to an array " + clientInputLine);
+           requestLines.add(clientInputLine);
+       }
+
        String[] uriParsed = null;
        String uri = "/";
+       String[] request;
+       String clientInput;
+       String userAgentContent = null;
        try {
-           uriParsed = clientInput.split(" ")[1].split("/");
-           uri = clientInput.split(" ")[1];
-           System.out.println("req = " + uri + " uriParsed " + uriParsed[1] + " " + uriParsed[2]);
+
+           clientInput = requestLines.get(0); //the request line with GET
+
+           request = clientInput.split(" ");
+
+           uri = request[request.length-2]; //HTTP/1.1 at the end
+           uriParsed = uri.split("/");
+
+           for (int i = 0; i < requestLines.size(); i++) {
+               if ( requestLines.get(i).startsWith("User-Agent:") ) {
+                    userAgentContent = requestLines.get(i).split(" ")[1];
+               }
+           }
+
        } catch (Exception e) {
              System.out.println("client input event: " + e.getMessage());
        }
 
-       //Integer clientInputLength = uri.length()-1;
-
        boolean isEchoUri = (uriParsed != null && uriParsed.length == 3 && "echo".equals(uriParsed[1]) );
+       boolean isUserAgentUri = (uriParsed != null && uriParsed.length == 2 && "user-agent".equals(uriParsed[1]) && userAgentContent != null );
 
-       out = new PrintWriter(socket.getOutputStream(), true);
+       PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
        if ( "/".equals(uri)
-            || isEchoUri) {
-           if ( isEchoUri )
+            || isEchoUri
+            || isUserAgentUri
+       ) {
+           if ( isEchoUri ) {
                out.println("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + uriParsed[2].length() + "\r\n\r\n" + uriParsed[2]);
-           else
+           } else if (isUserAgentUri) {
+               out.println("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + userAgentContent.length() + "\r\n\r\n" + userAgentContent);
+           } else
                out.println("HTTP/1.1 200 OK\r\n\r\n");
        } else {
            out.println("HTTP/1.1 404 Not Found\r\n\r\n");
