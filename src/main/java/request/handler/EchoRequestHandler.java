4412b401e98b2ff4910123a5492e2a000708b2fc
lewap <lewap02@gmail.com>
2025-02-04 16:48:56 +0100

    CR8 gzip compression

--- src/main/java/request/handler/EchoRequestHandler.java
+++ src/main/java/request/handler/EchoRequestHandler.java
@@ -9,12 +9,25 @@ import request.Response;
 
 import server.SupportedContentEncoding;
 
+import java.io.*;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.zip.GZIPOutputStream;
 
 @Service
 @RequestHandler(path="echo")
 public class EchoRequestHandler implements IRequestHandler {
+
+    private void gzip(InputStream is, OutputStream os, int bufferSize) throws IOException {
+        GZIPOutputStream gzipOs = new GZIPOutputStream(os);
+        byte[] buffer = new byte[bufferSize];
+        int bytesRead = 0;
+        while ((bytesRead = is.read(buffer)) > -1) {
+            gzipOs.write(buffer, 0, bytesRead);
+        }
+        gzipOs.close();
+    }
 
     @Override
     public Response getResponse ( Request request ) {
@@ -30,21 +43,36 @@ public class EchoRequestHandler implements IRequestHandler {
             return new Response(HttpStatus.BAD_REQUEST, "No echo content given");
 
         responseHeaders.put("Content-Type", String.valueOf(MediaType.TEXT_PLAIN));
-        responseHeaders.put("Content-Length", String.valueOf(endpointParsed[2].length()));
 
         String[] acceptEncodingParsed = null;
+
+        String body = endpointParsed[2];
+        System.out.println("body as is: " + body);
 
         if ( requestHeader.get("Accept-Encoding") != null ) {
             acceptEncodingParsed = requestHeader.get("Accept-Encoding").split(",");
             if ( SupportedContentEncoding.isSupported(acceptEncodingParsed) ) {
                 responseHeaders.put("Content-Encoding", SupportedContentEncoding.GZIP.getValue());
+
+                ByteArrayOutputStream os = new ByteArrayOutputStream();
+                try {
+                    gzip(new ByteArrayInputStream(body.getBytes()), os, body.length());
+                } catch (IOException e) {
+                    return new Response(HttpStatus.INTERNAL_SERVER_ERROR, "Error encoding input: " + e.getMessage());
+                }
+
+                body = os.toString();
+
+                System.out.println("body compressed: " + body);
             }
         }
+
+        responseHeaders.put("Content-Length", String.valueOf(body.length()));
 
         response = new Response(
                 HttpStatus.OK,
                 responseHeaders,
-                endpointParsed[2]
+                body
         );
 
         return response;
